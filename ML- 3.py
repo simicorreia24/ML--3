234f489fb63f5785083ec6154f0edb8e0e9a7790
Simi Silvester Correia <66421852+simicorreia24@users.noreply.github.com>
2022-01-29 17:44:09 +0530

    Add files via upload

---
+++ ML- 3.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1.Explain the term machine learning, and how does it work? Explain two machine learning applications in the business world. What are some of the ethical concerns that machine learning applications could raise?
+
+# In[ ]:
+
+
+# Machine learning is a data analytics technique that teaches computers to do what comes naturally to humans and animals
+# Real-time chatbot systems, Fraud detection etc
+# Lack quality data, cost to innovation etc
+
+
+# ## 2. Describe the process of human learning:
+# ## i. Under the supervision of experts
+# 
+# ## ii. With the assistance of experts in an indirect manner
+# 
+# ## iii. Self-education
+
+# In[ ]:
+
+
+
+
+
+# ## 3. Provide a few examples of various types of machine learning.
+
+# In[ ]:
+
+
+
+
+
+# ## 4. Examine the various forms of machine learning.
+
+# In[ ]:
+
+
+
+
+
+# ## 5. Can you explain what a well-posed learning problem is? Explain the main characteristics that must be present to identify a learning problem properly.
+
+# In[ ]:
+
+
+
+
+
+# ## 6. Is machine learning capable of solving all problems? Give a detailed explanation of your answer.
+
+# In[ ]:
+
+
+
+
+
+# ## 7. What are the various methods and technologies for solving machine learning problems? Any two of them should be defined in detail.
+
+# In[ ]:
+
+
+
+
+
+# ## 8. Can you explain the various forms of supervised learning? Explain each one with an example application.
+
+# In[ ]:
+
+
+
+
+
+# ## 9. What is the difference between supervised and unsupervised learning? With a sample application in each region, explain the differences.
+
+# In[ ]:
+
+
+
+
+
+# ## 10. Describe the machine learning process in depth.
+# 
+# ## a. Make brief notes on any two of the following:
+# 
+# ## MATLAB is one of the most widely used programming languages.
+# 
+# ## ii. Deep learning applications in healthcare
+# 
+# ## iii. Study of the market basket
+# 
+# ## iv. Linear regression (simple)
+
+# In[ ]:
+
+
+
+
+
+# ## 11. Make a comparison between:-
+# 
+# ## 1. Generalization and abstraction
+# 
+# ## 2. Learning that is guided and unsupervised
+# 
+# ## 3. Regression and classification
+
+# In[ ]:
+
+
+
+
